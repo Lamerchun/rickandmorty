a4113edc756c93c37b9dda3b4692f6efd72ec4d3
Tuan Nguyen <3486697+Lamerchun@users.noreply.github.com>
2022-02-02 15:36:25 +0100

    move results to component

---
+++ App.Server/_Client/src/components/results.vue
@@ -0,0 +1,45 @@
+﻿<template>
+	<table>
+		<tr>
+			<th>
+				ID
+			</th>
+			<th>
+				Name
+			</th>
+			<th class="text-center hidden sm:table-cell">
+				Episodes
+			</th>
+			<th class="hidden sm:table-cell">
+				Origin
+			</th>
+		</tr>
+		<tr v-for="entry in results">
+			<td>
+				{{entry.id}}
+			</td>
+			<td>
+				<ul class="flex flex-col gap-1">
+					<li>
+						{{entry.name}}
+					</li>
+					<li>
+						{{entry.species}}, {{entry.status}}
+					</li>
+				</ul>
+			</td>
+			<td class="text-center hidden sm:table-cell">
+				{{entry.episode?.length}}
+			</td>
+			<td class="hidden sm:table-cell">
+				{{entry.origin.name}}
+			</td>
+		</tr>
+	</table>
+</template>
+
+<script>
+	export default {
+		props: ['results']
+	}
+</script>
--- App.Server/_Client/src/home.vue
+++ App.Server/_Client/src/home.vue
@@ -43,43 +43,7 @@
 
 		<div v-if="results"
 			 class="w-full border border-black rounded-md overflow-hidden">
-			<table>
-				<tr>
-					<th>
-						ID
-					</th>
-					<th>
-						Name
-					</th>
-					<th class="text-center hidden sm:table-cell">
-						Episodes
-					</th>
-					<th class="hidden sm:table-cell">
-						Origin
-					</th>
-				</tr>
-				<tr v-for="entry in results">
-					<td>
-						{{entry.id}}
-					</td>
-					<td>
-						<ul class="flex flex-col gap-1">
-							<li>
-								{{entry.name}}
-							</li>
-							<li>
-								{{entry.species}}, {{entry.status}}
-							</li>
-						</ul>
-					</td>
-					<td class="text-center hidden sm:table-cell">
-						{{entry.episode?.length}}
-					</td>
-					<td class="hidden sm:table-cell">
-						{{entry.origin.name}}
-					</td>
-				</tr>
-			</table>
+			<us-results :results="results" />
 		</div>
 	</div>
 </template>
@@ -90,11 +54,13 @@
 
 	import usInput from './components/input.vue'
 	import usPager from './components/pager.vue'
+	import usResults from './components/results.vue'
 
 	export default {
 		components: {
 			usInput,
-			usPager
+			usPager,
+			usResults
 		},
 
 		setup() {
