3a3953820c68f5df71020b1186889eecc1234a6c
Tuan Nguyen <3486697+Lamerchun@users.noreply.github.com>
2022-02-05 03:16:04 +0100

    split tests

--- App.Tests/E2e/SearchTests.cs
+++ App.Tests/E2e/SearchTests.cs
@@ -6,6 +6,86 @@ namespace Tests.E2e.Public;
 public class SearchTests : TestBase
 {
 	[Test]
+	public void Home_GraphQL_Live()
+	{
+		// Arrange
+		var homePage = new HomePage(Driver);
+		homePage.GotoPage();
+
+		// Act, Assert
+		homePage.ClickGraphQL();
+		homePage.ClickLive();
+		homePage.ResetInput();
+
+		homePage.TypeSearch("ri");
+		E2eHelper.WaitApi();
+		WaitDisplayed(HomePage.Suggestions);
+
+		homePage.EnterInput();
+		WaitDisplayed(HomePage.Results);
+	}
+
+	[Test]
+	public void Home_GraphQL_Proxy()
+	{
+		// Arrange
+		var homePage = new HomePage(Driver);
+		homePage.GotoPage();
+
+		// Act, Assert
+		homePage.ClickGraphQL();
+		homePage.ClickProxy();
+		homePage.ResetInput();
+
+		homePage.TypeSearch("ri");
+		E2eHelper.WaitApi();
+		WaitDisplayed(HomePage.Suggestions);
+
+		homePage.EnterInput();
+		WaitDisplayed(HomePage.Results);
+	}
+
+	[Test]
+	public void Home_REST_Live()
+	{
+		// Arrange
+		var homePage = new HomePage(Driver);
+		homePage.GotoPage();
+
+		// Act, Assert
+		homePage.ClickREST();
+		homePage.ClickLive();
+		homePage.ResetInput();
+
+		homePage.TypeSearch("ri");
+		E2eHelper.WaitApi();
+		WaitDisplayed(HomePage.Suggestions);
+
+		homePage.EnterInput();
+		WaitDisplayed(HomePage.Results);
+	}
+
+	[Test]
+	public void Home_REST_Proxy()
+	{
+		// Arrange
+		var homePage = new HomePage(Driver);
+		homePage.GotoPage();
+
+		// Act, Assert
+		homePage.ClickREST();
+		homePage.ClickLive();
+		homePage.ResetInput();
+
+		homePage.TypeSearch("ri");
+		E2eHelper.WaitApi();
+		WaitDisplayed(HomePage.Suggestions);
+
+		homePage.EnterInput();
+		WaitDisplayed(HomePage.Results);
+	}
+
+	[Test]
 	[TestCaseSource(typeof(SearchSource), nameof(SearchSource.Prepare))]
 	public void Home_Shows_Suggestions_And_Results(Action<HomePage> prepare)
 	{
--- App.Tests/_Src/E2e/E2eHelper.cs
+++ App.Tests/_Src/E2e/E2eHelper.cs
@@ -8,5 +8,5 @@ public static class E2eHelper
 		=> Thread.Sleep(250);
 
 	public static void WaitApi()
-		=> Thread.Sleep(1_500);
+		=> Thread.Sleep(500);
 }
