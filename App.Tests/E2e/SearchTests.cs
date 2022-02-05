6d5b43e9c9fb2440e53defc180f70df3784496ed
Tuan Nguyen <3486697+Lamerchun@users.noreply.github.com>
2022-02-05 03:19:17 +0100

    remove testcase

--- App.Tests/E2e/SearchTests.cs
+++ App.Tests/E2e/SearchTests.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using System;
 
 namespace Tests.E2e.Public;
 
@@ -83,52 +82,5 @@ public class SearchTests : TestBase
 
 		homePage.EnterInput();
 		WaitDisplayed(HomePage.Results);
-	}
-
-	[Test]
-	[TestCaseSource(typeof(SearchSource), nameof(SearchSource.Prepare))]
-	public void Home_Shows_Suggestions_And_Results(Action<HomePage> prepare)
-	{
-		// Arrange
-		var homePage = new HomePage(Driver);
-		homePage.GotoPage();
-
-		// Act, Assert
-		prepare(homePage);
-		homePage.ResetInput();
-
-		homePage.TypeSearch("ri");
-		E2eHelper.WaitApi();
-		WaitDisplayed(HomePage.Suggestions);
-
-		homePage.EnterInput();
-		WaitDisplayed(HomePage.Results);
-	}
-
-	public class SearchSource
-	{
-		public static Action<HomePage>[] Prepare =
-			new Action<HomePage>[] {
-				homePage =>
-				{
-					homePage.ClickGraphQL();
-					homePage.ClickLive();
-				},
-				homePage =>
-				{
-					homePage.ClickGraphQL();
-					homePage.ClickProxy();
-				},
-				homePage =>
-				{
-					homePage.ClickREST();
-					homePage.ClickLive();
-				},
-				homePage =>
-				{
-					homePage.ClickREST();
-					homePage.ClickProxy();
-				}
-			};
 	}
 }
